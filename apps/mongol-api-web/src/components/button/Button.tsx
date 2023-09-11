9c7e9cfa6d3692b79dab42669fdeb68c2b490fc4
Duuree Ulziiduuren <duurenbayarulz@gmail.com>
2023-09-11 16:41:55 -0500

    feat(mongol-api-web): touch up button component

--- apps/mongol-api-web/src/components/button/Button.tsx
+++ apps/mongol-api-web/src/components/button/Button.tsx
@@ -1,17 +1,39 @@
-import {Button as MuiButton}  from '@mui/material';
+import MuiButton from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
 
-export type ButtonSize = 'small'| 'medium';
-export interface ButtonProps {
+export type ButtonSize = 'small' | 'medium';
+export interface BaseButtonProps {
   children: React.ReactNode;
   size?: ButtonSize;
-  fullWidth?: boolean
+  fullWidth?: boolean;
   onClick?: () => void;
 }
 
-export const Button = ({
-  children, ...rest
-}: ButtonProps) => {
+const StyledButton = styled(MuiButton)<BaseButtonProps>(({ theme, size }) => {
+  return {
+    borderRadius: theme.spacing(4),
+    padding: `${theme.spacing(6)} ${theme.spacing(12)}`,
+    fontSize: theme.spacing(8),
+    fontWeight: 500,
+    lineHeight: 'normal',
+    textTransform: 'none',
+    color: '#fff',
+    backgroundColor: '#00C0CC',
+    '&:hover': {
+      backgroundColor: '#002762',
+    },
+
+    ...(size === 'small' && {
+      padding: `${theme.spacing(3)} ${theme.spacing(6)}`,
+      fontSize: theme.spacing(6),
+    }),
+  };
+});
+
+export const Button = ({ children, ...rest }: BaseButtonProps) => {
   return (
-      <MuiButton {...rest}>{children}</MuiButton>
+    <StyledButton disableRipple fullWidth {...rest}>
+      {children}
+    </StyledButton>
   );
-}
+};
