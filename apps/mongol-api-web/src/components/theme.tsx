3fe28c8b78fc25da2e59dc560e82291bcabad134
Duuree Ulziiduuren <duurenbayarulz@gmail.com>
2023-09-10 21:58:09 -0500

    feat(web): created theme

---
+++ apps/mongol-api-web/src/components/theme.tsx
@@ -0,0 +1,61 @@
+/* eslint-disable unicorn/filename-case */
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import { FC, PropsWithChildren } from 'react';
+
+declare module '@mui/material/styles' {
+  interface PaletteColor {
+    100?: string;
+    200?: string;
+    300?: string;
+    400?: string;
+    500?: string;
+  }
+
+  interface SimplePaletteColorOptions {
+    100?: string;
+    200?: string;
+    300?: string;
+    400?: string;
+    500?: string;
+  }
+}
+
+const theme = createTheme({
+  spacing: 4,
+  palette: {
+    primary: {
+      100: '#F0F6FF',
+      200: '#83BDFF',
+      300: '#1E70EB',
+      400: '#0043A8',
+      500: '#0B253C',
+    },
+    common: {
+      black: '#000',
+      white: '#fff',
+    },
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1280,
+      xl: 1536,
+    },
+  },
+  typography: {
+    fontFamily: 'Helvetica Neue, Helvetica, Arial, sans-serif',
+  },
+});
+
+const Theme: FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
+};
+
+export default Theme;
