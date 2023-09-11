b084d4209e3761ad603d47cc19109b4116973bc7
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2023-09-11 15:40:25 -0500

    feat(mongol-api-web): implemented button component

--- apps/mongol-api-web/src/components/button/Button.tsx
+++ apps/mongol-api-web/src/components/button/Button.tsx
@@ -1,3 +1,17 @@
-export const Button = () => {
-  return <div>This is Button</div>;
-};
+import {Button as MuiButton}  from '@mui/material';
+
+export type ButtonSize = 'small'| 'medium';
+export interface ButtonProps {
+  children: React.ReactNode;
+  size?: ButtonSize;
+  fullWidth?: boolean
+  onClick?: () => void;
+}
+
+export const Button = ({
+  children, ...rest
+}: ButtonProps) => {
+  return (
+      <MuiButton {...rest}>{children}</MuiButton>
+  );
+}
