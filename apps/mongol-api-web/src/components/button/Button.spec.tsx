a433e6dc1367c866d5d269d9da31f1ca75b3b5e2
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2023-09-12 21:39:01 -0500

    test(mongol-api-web): wrote header, footer and button unit tests

--- apps/mongol-api-web/src/components/button/Button.spec.tsx
+++ apps/mongol-api-web/src/components/button/Button.spec.tsx
@@ -3,9 +3,14 @@ import { render } from '@testing-library/react';
 
 import { Button } from './Button';
 
-describe('Index', () => {
-  it('should render successfully', () => {
+describe('Button', () => {
+  it('1. Should render successfully', () => {
     const { baseElement } = render(<Button> hello</Button>);
+    expect(baseElement).toBeTruthy();
+  });
+
+  it('2. Should render with props', () => {
+    const { baseElement } = render(<Button size="small">Small Button</Button>);
     expect(baseElement).toBeTruthy();
   });
 });
--- apps/mongol-api-web/src/components/header/Header.spec.tsx
+++ apps/mongol-api-web/src/components/header/Header.spec.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-
 import { Header } from './Header';
 
-describe('Index', () => {
-  it('should render successfully', () => {
+describe('Header', () => {
+  it('1. Should render successfully', () => {
     const { baseElement } = render(<Header />);
+    expect(baseElement).toBeTruthy();
+  });
+
+  it('2. Should render with props', () => {
+    const { baseElement } = render(<Header colorMode="dark" />);
     expect(baseElement).toBeTruthy();
   });
 });
