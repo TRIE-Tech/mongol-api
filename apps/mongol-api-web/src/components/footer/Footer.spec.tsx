da28c3619e919853da371d9ad8277800ae908843
Anujin Buyannemekh <anujinbuyannemekh26@gmail.com>
2023-09-12 21:39:01 -0500

    test(mongol-api-web): wrote header, footer and button unit tests

--- apps/mongol-api-web/src/components/footer/Footer.spec.tsx
+++ apps/mongol-api-web/src/components/footer/Footer.spec.tsx
@@ -3,9 +3,14 @@ import { render } from '@testing-library/react';
 
 import { Footer } from './Footer';
 
-describe('Index', () => {
-  it('should render successfully', () => {
+describe('Footer', () => {
+  it('1. Should render successfully', () => {
     const { baseElement } = render(<Footer />);
+    expect(baseElement).toBeTruthy();
+  });
+
+  it('2. Should render with props', () => {
+    const { baseElement } = render(<Footer colorMode="dark" />);
     expect(baseElement).toBeTruthy();
   });
 });
