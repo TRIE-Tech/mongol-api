d8236b3ed208be19a26dde72099461522ed42a9e
Elbeg Amar <elbeg@nestsolutions.llc>
2023-10-24 15:21:00 -0700

    feat(mongol-api-web): fixed icons defaults

--- apps/mongol-api-web/src/icons/ProvinceIcon.tsx
+++ apps/mongol-api-web/src/icons/ProvinceIcon.tsx
@@ -5,13 +5,13 @@ export const ProvinceIcon = (props: BaseIconProps) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width={56 || width}
-      height={40 || height}
+      width={width ?? 56}
+      height={height ?? 40}
       fill="none"
       {...props}
     >
       <path
-        fill={'#D0D7E2' || color}
+        fill={color ?? '#D0D7E2'}
         d="M37.333 39.512 18.667 34.96 0 40V4.932L18.667.488 37.333 5.04 56 0v35.068l-18.667 4.444Zm-3.11-5.312V8.835L21.777 5.799v25.366L34.222 34.2Z"
       />
     </svg>
