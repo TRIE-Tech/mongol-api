e7088077f44bb151cee54a264bb34a95f1e225ab
Elbeg Amar <elbeg@nestsolutions.llc>
2023-10-24 15:07:48 -0700

    fix(mongol-api): testing other services

--- apps/mongol-api-images-service/pages/index.tsx
+++ apps/mongol-api-images-service/pages/index.tsx
@@ -2,7 +2,7 @@ export const Index = () => {
   return (
     <div className="container">
       <div id="welcome">
-        <h1>Welcome to Mongol-Api-Image-Service 👋</h1>
+        <h1>Welcome to Mongol-Api-Images-Service 👋</h1>
       </div>
     </div>
   );
