710f0ea67a03db2a4b9d15fbbe53934a26584846
Duuree Ulziiduuren <duurenbayarulz@gmail.com>
2023-10-26 13:45:25 -0500

    feat(mongol-api-web): changed favicon

--- apps/mongol-api-web/src/pages/_app.tsx
+++ apps/mongol-api-web/src/pages/_app.tsx
@@ -9,6 +9,7 @@ const CustomApp = ({ Component, pageProps }: AppProps) => {
       <Theme>
         <Head>
           <title>Welcome to mongol-api-web!</title>
+          <link rel="icon" href="/logo.png" />
         </Head>
         <main className="app">
           <Component {...pageProps} />
